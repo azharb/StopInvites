f72ef0bc0fc113ba4af5944a11987287bc9f1535
AzharB <azhar.bandeali@gmail.com>
2013-01-11 21:08:48 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stopinvites::Application.config.secret_token = '00d7237b390def1709cc152e377d6515c1bfbaec347b7bdbf6147851c8497760738fa8bf953ee67c767650dcd1214ecf4bb0ba4bc2f9a3be380169b8de75cf9a'
